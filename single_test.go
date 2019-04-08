46c6a3b435f16f3e7c5a970e2ba7a2e12ae35ff7
hezhanbang <liyoubdu@qq.com>
2019-04-08 15:51:49 +0800

    2

--- single_test.go
+++ single_test.go
@@ -1,10 +1,22 @@
 package SingleInstance
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRun(t *testing.T) {
-	HelloTest()
-	IsSingle()
+	//HelloTest()
+
+	for i := 0; i < 5; i++ {
+		ok := IsSingle("__yourTest___")
+		if ok {
+			fmt.Printf("current process is only single, can run now\n")
+		} else {
+			fmt.Printf("too many processes are runing, exit now\n")
+		}
+	}
+	fmt.Printf("done to check\n")
 
 	t.Logf("---------DONE------")
 }

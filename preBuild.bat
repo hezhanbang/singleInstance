1b7666cd801ee7ce023f2657ec8855d5a92b31cd
hezhanbang <liyoubdu@qq.com>
2019-04-08 15:29:15 +0800

    2

---
+++ preBuild.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set workDir=%cd%
+set singleDir=%~dp0
+cd %singleDir%
+
+del -f single.go
+copy lib\singleWindows.go single.go
+if %errorlevel% neq 0 goto ERROR
+exit /b 0
+
+:ERROR
+echo "fail to do preBuild.bat in lib [SingleInstance]!"
+cd /d %workDir%
+exit /b 1

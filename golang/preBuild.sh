46aec273151402b160cd03b0b34c4d58763c72b9
liyou <liyoubdu@qq.com>
2019-05-24 19:00:59 +0800

    copy to dst

--- golang/preBuild.sh
+++ golang/preBuild.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+option=$1
+case "$option" in
+	dst=*) value=`echo "$option" | sed -e 's/[-_a-zA-Z0-9]*=//'` ;;
+	DST=*) value=`echo "$option" | sed -e 's/[-_a-zA-Z0-9]*=//'` ;;
+	   *) value="" ;;
+esac
+echo $value
+
+dstDir=
+if [ -n "$value" ]; then
+	dstDir=$value
+fi
+
 workDir=$(pwd)
 singleDir=$(dirname $(readlink -f $0))
 cd $singleDir
 
-rm -rf single.go
-cp lib/singleLinux.go single.go
+if [ -n "$dstDir" ]; then
+	rm -rf $dstDir
+	mkdir -p $dstDir
+	cp lib/singleLinux.go $dstDir/single.go
+else
+	rm -rf single.go
+	cp lib/singleLinux.go single.go
+fi
 
 retVal=$?
 cd $workDir

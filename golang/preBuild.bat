2bddb5e8e5fd2b6c7e3418bd38974e505954e3be
liyou <liyoubdu@qq.com>
2019-05-24 18:50:53 +0800

    2

--- golang/preBuild.bat
+++ golang/preBuild.bat
@@ -4,9 +4,14 @@ set workDir=%cd%
 set singleDir=%~dp0
 cd %singleDir%
 
-IF EXIST single.go del -F single.go
-copy lib\singleWindows.go single.go
-
+set DST_DIR=%1
+IF "%DST_DIR%"=="" (
+	IF EXIST single.go del -F single.go
+	copy lib\singleWindows.go single.go
+)ELSE (
+	mkdir "%DST_DIR%"
+	copy lib\singleWindows.go "%DST_DIR%\single.go"
+)
 set retVal=%errorlevel%
 cd /d %workDir%
 

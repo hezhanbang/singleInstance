9992392e70139db846b34e70326b1b9083448969
liyou <liyoubdu@qq.com>
2019-05-25 09:17:18 +0800

    get all arguments

--- golang/preBuild.sh
+++ golang/preBuild.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-option=$1
-case "$option" in
-	dst=*) value=`echo "$option" | sed -e 's/[-_a-zA-Z0-9]*=//'` ;;
-	DST=*) value=`echo "$option" | sed -e 's/[-_a-zA-Z0-9]*=//'` ;;
-	   *) value="" ;;
-esac
-echo $value
+## e.g. sh preBuild.sh --dst=/home/heb/install
 
-dstDir=
-if [ -n "$value" ]; then
-	dstDir=$value
-fi
+opt=
+
+for option
+do
+    opt="$opt `echo $option | sed -e \"s/\(--[^=]*=\)\(.* .*\)/\1'\2'/\"`"
+
+    case "$option" in
+        -*=*) value=`echo "$option" | sed -e 's/[-_a-zA-Z0-9]*=//'` ;;
+           *) value="" ;;
+    esac
+
+    case "$option" in
+        --dst=*)                       DST_DIR="$value"             ;;
+	esac
+done
 
 workDir=$(pwd)
 singleDir=$(dirname $(readlink -f $0))
 cd $singleDir
 
-if [ -n "$dstDir" ]; then
-	rm -rf $dstDir
-	mkdir -p $dstDir
-	cp lib/singleLinux.go $dstDir/single.go
+if [ -n "$DST_DIR" ]; then
+	rm -rf $DST_DIR
+	mkdir -p $DST_DIR
+	cp lib/singleLinux.go $DST_DIR/single.go
 else
 	rm -rf single.go
 	cp lib/singleLinux.go single.go
